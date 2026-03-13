bd402e97fa9c94541a4e2aeb0497c96adfabe8b3
sudarkingeorgiy <sudarkin.gi@phystech.edu>
2026-03-14 01:30:58 +0300

    add changeable fields and logo

--- static/js/script.js
+++ static/js/script.js
@@ -6,3 +6,33 @@ function updateTime(){
     }
     updateTime();
     setInterval(updateTime, 1000);
+
+const photoInput = document.querySelectorAll('.photo input');
+
+photoInput.forEach(function(input) {
+    input.addEventListener('change', function() {
+        const file = input.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                const preview = input.parentElement.querySelector('.photo-preview');
+                const chosen = input.parentElement.querySelector(".file-input");
+                preview.src = e.target.result;
+                preview.style.display = 'block';
+                if (chosen) {
+                    chosen.style.display = 'none';
+                }
+                preview.addEventListener('contextmenu', function() {
+                    e.preventDefault();
+                    preview.src = '';
+                    preview.style.display = 'none';
+                    input.value = '';
+                    if (chosen) {
+                        chosen.style.display = '';
+                    }
+                });
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+});
